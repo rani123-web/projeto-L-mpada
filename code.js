ceab54804ecb7fa412ebefcbfddf834a847d7535
Raniele Santino <r7sansantino@gmail.com>
2024-08-20 11:29:04 -0300

    commit

---
+++ code.js
@@ -0,0 +1,21 @@
+const turnOn = document.getElementById("turnOn");
+
+const turnOff = document.getElementById("turnOff");
+
+const lamp = document.getElementById("lamp");
+
+function lampOn() {
+  lamp.src="./ligada.jpg"
+}
+
+function lampOff() {
+  lamp.src="./desligada.jpg"
+}
+
+turnOn.addEventListener("click", lampOn);
+
+turnOff.addEventListener("click", lampOff);
+
+lamp.addEventListener("mouseover", lampOn)
+
+lamp.addEventListener("mouseleave", lampOff)
